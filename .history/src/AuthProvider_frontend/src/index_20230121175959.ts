10cfff527fdbbebe8b3add9523345935b8088219
Alejandro Andrade <alejandrodanielandrade@gmail.com>
2023-01-21 18:39:29 -0500

    demo

--- .history/src/AuthProvider_frontend/src/index_20230121175959.ts
+++
@@ -1,5 +0,0 @@
-import { AuthClient } from "@dfinity/auth-client";
-import { Actor, Identity } from "@dfinity/agent";
-
-
-init();
--- .history/src/AuthProvider_frontend/src/index_20230121180006.ts
+++
@@ -1,4 +0,0 @@
-import { AuthClient } from "@dfinity/auth-client";
-
-
-init();
